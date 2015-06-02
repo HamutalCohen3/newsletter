17feea890c3b870202c388ae371ca9cfa9a97a92
Hamutal Cohen <hamutalcohen3@gmail.com>
2015-06-03 00:20:39 +0300

    6 - media queries and scroll effects

--- newsletter.js
+++ newsletter.js
@@ -6,17 +6,34 @@ $(document).ready(function(){
 	
 	
 });
-$(".news .btn-form-display").click(function(){
-	$(".matzov-for-matzov-form").hide();
-	$(".news-form").toggle(300);
-})
+/*$(".news .btn-form-display").click(function(){
+	
+})*/
+$(".news .btn-form-display").click(function() {
+	$("#matzov-for-matzov-form .form-container").hide();
+	$("#news-form .form-container").show(300);
+    $('html, body').animate({
+        scrollTop: $("#news-form").offset().top
+	}, 400);
+});
 $(".matzov-for-matzov .btn-form-display").click(function(){
-	$(".news-form").hide();
-	$(".matzov-for-matzov-form").toggle(300);
+	$("#news-form .form-container").hide();
+	$("#matzov-for-matzov-form .form-container").show(300);
+	$("html, body").animate({
+		scrollTop: $("#matzov-for-matzov-form").offset().top
+	}, 400);
 })
 $(".jobs .btn-form-display").click(function(){
-	$(".jobs-form").toggle(300);
+	$("#jobs-form .form-container").show(300);
+	$("html, body").animate({
+		scrollTop: $("#jobs-form").offset().top,
+		}, 400
+	);
 })
 $(".btn-cancel").click(function(){
+	$("html, body").animate({
+		scrollTop: $(this).parents(".form-container").offset().top
+	}, 300);
 	$(this).parents(".form-container").hide(300);
+	return false;
 })
